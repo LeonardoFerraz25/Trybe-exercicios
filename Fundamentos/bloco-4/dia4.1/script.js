da09ff1445296da686472b77c582ac67aa7b6c6c
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-01-28 12:54:35 -0300

    iniciando no JavaScript

---
+++ Fundamentos/bloco-4/dia4.1/script.js
@@ -0,0 +1,5 @@
+let age = 21
+let nome = "Leonardo"
+
+console.log(age)
+console.log(nome)
---
+++ Fundamentos/bloco-4/dia4.1/script02.js
@@ -0,0 +1,10 @@
+const myname = "Leonardo";
+const birthCity = "São Gonçalo";
+let birthYear = '2000';
+birthYear = "2030"
+
+
+console.log(myname);
+console.log(birthCity);
+console.log(birthYear);
+
