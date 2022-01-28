12324bf6b6dcd556a602ece4d36fa4089404450c
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-01-28 16:36:10 -0300

    switch e case

---
+++ Fundamentos/bloco-4/dia4.1/switch_case.js
@@ -0,0 +1,19 @@
+let trafficLight = "verde";
+
+switch (trafficLight) {
+    case "vermelho":
+        console.log("Pare, sinal vermelho")
+        break;
+    
+    case "amarelo":
+        console.log("Atenção, sinal Amarelo")
+        break;
+
+    case "verde":
+        console.log("Sinal Verde,Siga em Frente")
+        break;
+    
+    default:
+        console.log("valor não identificado")
+
+}
---
+++ Fundamentos/bloco-4/dia4.1/teste.js
@@ -0,0 +1,15 @@
+let candidata = "reprovado";
+
+switch (candidata) {
+    case "aprovado":
+        console.log("Aprovado")
+    break;
+    case "lista":
+        console.log("lista de espera")
+    break;
+    case "reprovado":
+        console.log("Reprovado")
+    break;
+    default:
+        console.log("não se aplica")
+}
