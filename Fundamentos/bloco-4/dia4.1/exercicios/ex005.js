55438a5d2c2f62d4ab1c6dcaa5674cb59bd76e0f
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-01-28 23:39:11 -0300

    add ex005

---
+++ Fundamentos/bloco-4/dia4.1/exercicios/ex005.js
@@ -0,0 +1,12 @@
+const a = 60;
+const b = 60;
+const c = 60;
+let soma = a+b+c;
+
+if(soma == 180){
+    console.log("true")
+}else if(soma != 180){
+    console.log("false")
+}else{
+    console.log(ErrO)
+}
