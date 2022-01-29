dde5db308a25b77acbe548ac837fa5a95bb32227
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-01-28 22:49:37 -0300

    add ex001

---
+++ Fundamentos/bloco-4/dia4.1/exercicios/ex001.js
@@ -0,0 +1,14 @@
+const a = 8;
+const b = 2;
+let somar = a+b;
+let diminuir = a-b;
+let multiplicar = a*b;
+let dividir = a/b;
+let modulo = a%b;
+
+
+console.log(`a Soma entre ${a} + ${b} é = ${somar}`);
+console.log(`a Subtração entre ${a} - ${b} é = ${diminuir}`);
+console.log(`a Multiplicação entre ${a} * ${b} é = ${multiplicar}`);
+console.log(`a Divisão entre ${a} / ${b} é = ${dividir}`);
+console.log(`a Modulação entre ${a} % ${b} é = ${modulo}`);
