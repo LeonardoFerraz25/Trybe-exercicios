e08ef033662fc47773fb39714ab47cf7edbe20f0
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-01-31 20:34:44 -0300

    add ex007

--- Fundamentos/bloco-4/dia4.2/exercicios/ex007.js
+++ Fundamentos/bloco-4/dia4.2/exercicios/ex007.js
@@ -1,10 +1,14 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 13];
+//let numbers = [70, 8, 100, 2];
 let impar = 0;
 
-for (let i = 0; i < numbers.length; i++ ){
-    if(numbers[1]%2 == 0){
-        ;
+for (let i = 0; i < numbers.length; i +=1 ){
+    if(numbers[i]%2 != 0){
+        impar += 1;
     }
-    
 }
-console.log(impar);
+if(impar == 0){
+    console.log("nenum valor impar")
+}else{
+    console.log(`${impar} numeros impares`);
+}
