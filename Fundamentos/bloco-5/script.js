13424ff428e38fcc837e4b12a1cbb509b4d4cd21
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-02-09 16:14:45 -0300

    add querySelecto exercices

---
+++ Fundamentos/bloco-5/script.js
@@ -0,0 +1,19 @@
+const header = document.querySelector("#header-container");
+header.style.backgroundColor = "green";
+
+const emergencyTask = document.querySelector(".emergency-tasks");
+emergencyTask.style.backgroundColor = "grey"
+
+const noEmergencyTask = document.querySelector(".no-emergency-tasks");
+noEmergencyTask.style.backgroundColor = "RoyalBlue";
+
+const titulo = document.querySelectorAll("h3");
+titulo[0].style.backgroundColor = "RoyalBlue";
+titulo[1].style.backgroundColor = "RoyalBlue";
+titulo[2].style.backgroundColor = "grey";
+titulo[3].style.backgroundColor = "grey";
+
+const footer = document.querySelector("#footer-container");
+footer.style.backgroundColor = "DarkGreen";
+
+
