a7163b3a180f9dd811e28cdfb2ef5638e8543c6a
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-02-11 18:41:34 -0300

    add ex

--- Fundamentos/bloco-5/dia5.3/conteudo/main.js
+++ Fundamentos/bloco-5/dia5.3/conteudo/main.js
@@ -38,20 +38,39 @@ function addClass(origem){
     }
 }
 
+
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 
+input.addEventListener("input", function(texto) {
+    const caixa = document.querySelector('.tech');
+    caixa.innerText = texto.target.value;
+
+})
+
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+const top3 = document.querySelector('#my-spotrybefy');
+top3.addEventListener("dblclick", function () {
+    window.location.replace('https://leonardoferraz25.github.io/portifolio/')
+} )
 // redirecione para alguma página;
 
 // 4.1. Que tal redirecionar para seu portfólio?
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+top3.addEventListener('mouseenter', function (){
+    top3.style.color = "#2fc18c" ;
+    top3.style.cursor = "pointer";
+})
+top3.addEventListener('mouseleave', function (){
+    top3.style.color = "#eee" ;
+})
+
 
 // Segue abaixo um exemplo do uso de event.target:
 
