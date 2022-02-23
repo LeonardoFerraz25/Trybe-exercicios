bcc4421b7385277a2d92b316fa0e9010ec01ecb8
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-02-23 10:49:00 -0300

    bloco 7

--- Fundamentos/bloco-7/dia7.1/execicios/script.js
+++ Fundamentos/bloco-7/dia7.1/execicios/script.js
@@ -23,3 +23,4 @@ const testingScope = (escopo) => {
   }
 }
 testingScope(true);
+testingScope(false);
---
+++ Fundamentos/bloco-7/dia7.1/execicios2/script.js
@@ -0,0 +1,9 @@
+function sortfunction(a, b){
+  return (a - b) //faz com que o array seja ordenado numericamente e de ordem crescente.
+}
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort(sortfunction);
+// Seu código aqui.
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
