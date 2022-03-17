be6298e82c2e76f78c1558626d32f10b33fcc9ae
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-03-17 18:39:21 -0300

    cotaçao das crypto moedas em real

--- Fundamentos/bloco-9/dia9.2/ex02/api.script.js
+++ Fundamentos/bloco-9/dia9.2/ex02/api.script.js
@@ -10,15 +10,29 @@ const fetchCoins = async () => {
   return coins;
 }
 
+const fetchCotation = async () => {
+  const url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json`; 
+ 
+  const usdCurrencies = await fetch(url)
+    .then(response => response.json())
+    .then(data => data.usd)
+    .catch(erro => erro.toString());
+
+  return usdCurrencies;
+}
+
 const mostrar = async () => {
   const coins = await fetchCoins();
+  const currencies = await fetchCotation();
+
   const lista = document.querySelector('#moedas')
   coins.forEach((moeda, index) => {
     if (index <= 10) {
       const newli = document.createElement('li');
-  
       const usdPrice = Number(moeda.priceUsd);
-      let info = `${moeda.name} (${moeda.symbol}): <span>${usdPrice.toFixed(2)}</span>`;
+      const brlPrice = usdPrice * currencies.brl;
+
+      let info = `${moeda.name} (${moeda.symbol}): <span>R$:${brlPrice.toFixed(2)}</span>`;
       
       newli.innerHTML = info;
       lista.appendChild(newli);
